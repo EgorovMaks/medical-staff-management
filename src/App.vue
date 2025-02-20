5e1d477af38d243e07a5c3700d20c5d4061f768e
maxim-egorov <funt.egorov@gmail.com>
2025-02-20 17:00:38 +0500

    fix app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
+<script setup>
+import HeaderNav from "@/components/HeaderNav.vue";
+</script>
+
 <template>
   <HeaderNav />
   <router-view />
 </template>
-
-<script setup>
-import HeaderNav from "@/components/HeaderNav.vue";
-</script>
